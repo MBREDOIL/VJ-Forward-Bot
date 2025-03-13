571618d1aac4155fc0a38b9d02cbf7dd19e1b00b
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-03-13 19:18:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ class Config:
     API_ID = int(environ.get("API_ID", "22182189"))
     API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
+    BOT_SESSION = environ.get("BOT_SESSION", "frbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "fr-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", "6556141430"))
 
 # Don't Remove Credit Tg - @VJ_Botz
